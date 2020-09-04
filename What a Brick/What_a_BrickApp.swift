b259e45f6ed11e18db21292821f19ca5138d606f
MAJKFL <kubaflor23@gmail.com>
2020-09-04 17:29:16 +0200

    Initial Commit

---
+++ What a Brick/What_a_BrickApp.swift
@@ -0,0 +1,17 @@
+//
+//  What_a_BrickApp.swift
+//  What a Brick
+//
+//  Created by Kuba Florek on 04/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct What_a_BrickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
